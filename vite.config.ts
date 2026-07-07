c2f77ba8b3973d66e8933c74769f0abc5091d768
Lukman Hakim <106062417+lukyman86@users.noreply.github.com>
2026-07-07 16:25:14 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   server: {
     port: 3000,
   },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
-    tsConfigPaths(),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
